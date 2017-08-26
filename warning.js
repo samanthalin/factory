ecc491904dc0bead90d77a2813d1012a74de23cd
Webb Lu <tsung.ying.lu@gmail.com>
2017-08-26 16:55:44 +0800

    use vue.js to dynamic change the fetching url and view

--- warning.js
+++ warning.js
@@ -55,46 +55,102 @@ $(document).ready(function(){
   // `
   //   });
   // }
+  var corsProxyServer = 'https://blahdns-proxy-eusudefuvv.now.sh/';
+  var ntutCsieServer = 'http://140.124.184.204:8080/Cloud/'
+  var date = moment("2017-08-23").format('x')
+  var page = 0
+  //WSNalarm or Iotivityalarm
+  // var alarmType = 'Iotivityalarm'
+  
+  var result = {}
 
-  var urlWsnAlarm =
-    'https://blahdns-proxy-eusudefuvv.now.sh/http://140.124.184.204:8080/Cloud/WSNalarm/QueryByDate?epoch='+currentDate+'&page=0';
-    
-  var fetchDataWsnAlarm = url => {
-    return fetch(url).then(resp => resp.json()).then(function(data_wsn) {
-      console.log(data_wsn);
-      appendWsnAlarm(data_wsn);
-    });
+  var fetchDataWsnAlarm = (alarmType) => {
+    var url =
+    corsProxyServer + ntutCsieServer + alarmType +'/QueryByDate?epoch=' + date + '&page=' + page;
+    return fetch(url).then(resp => resp.json()).then(
+      function(resultArray) {
+        console.warn('ajax go')
+        switch (alarmType) {
+          case 'WSNalarm': {
+            result = resultArray.map((item, index) => {
+              return {
+                idx: 100 * page + index + 1,
+                time: moment(item.timeStamp*1000).format('YYYY/MM/DD HH:mm:ss'),
+                moto: String(item.mote),
+                temperature: String(item.temperature),
+                msg: item.message
+              }
+            })
+            break
+          }
+          case 'Iotivityalarm': {
+            result = resultArray.map((item, index) => {
+              return {
+                idx: 100 * page + index + 1,
+                time: item.timeStamp,
+                msg: item.message
+              }
+            })
+            break  
+          }
+        }
+
+        // result = data_wsn
+        console.warn(result)
+        example1.alarmType = alarmType
+        example1.items = result
+      }
+    );
   };
 
-  fetchDataWsnAlarm(urlWsnAlarm);
+  fetchDataWsnAlarm('WSNalarm');
+  var example1 = new Vue({
+    el: '#wsnAlarm',
+    data: {
+      items: result
+    }
+  })
 
-  function appendWsnAlarm(data_wsn) {
-    var div = document.querySelector("#wsnAlarm");
-    data_wsn.forEach(e => {
-      var temperature = String(e.temperature);
-      var mote = String(e.mote);
-      div.innerHTML += `
-    Time: ${moment(e.timeStamp*1000).format('YYYY/MM/DD HH:mm:ss')} &nbsp &nbsp
-    溫度感測器 ${mote}&nbsp &nbsp
-     ${e.message}(${temperature} &#8451;)
-    <hr>
-  `
-    });
-  }
+  // function appendWsnAlarm(data_wsn) {
+  //   var div = document.querySelector("#wsnAlarm");
+  //   data_wsn.forEach(e => {
+  //     var temperature = String(e.temperature);
+  //     var mote = String(e.mote);
+  //     div.innerHTML += `
+  //   Time: ${moment(e.timeStamp*1000).format('YYYY/MM/DD HH:mm:ss')} &nbsp &nbsp
+  //   溫度感測器 ${mote}&nbsp &nbsp
+  //    ${e.message}(${temperature} &#8451;)
+  //   <hr>
+  // `
+  //   });
+  // }
+  var selectorChangeHandler = new Vue({
+    el: '#select_cat',
+    data: {
+      name: 'Vue.js'
+    },
+    // define methods under the `methods` object
+    methods: {
+      greet: function (event) {
+        console.warn(event.target.value)
+        fetchDataWsnAlarm(event.target.value)
+      }
+    }
+  })
 
-  //
-  console.log($('#select_cat').val());
   
-  var selectA = $('#select_cat, #select_time')
-  selectA.on('click, change', regenerateData)
+  // console.log($('#select_cat').val());
+  
+  // var selectA = $('#select_cat, #select_time')
+  // selectA.on('click, change', regenerateData)
 
-  function regenerateData (){
-    var v = $('#select_cat option:selected').val();
-  console.log(v);
-  }
+  // function regenerateData (){
+  //   var v = $('#select_cat option:selected').val();
+  // console.log(v);
+  // }
 
-  $('button').click(function(){
+  // $('button').click(function(){
 
-  });
+  // });
 
 });
