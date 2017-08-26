839c196946a8fd97c4dedd8fa0324f9c31b54fbf
samantha lin <samanthalin1992@gmail.com>
2017-08-26 21:58:22 +0800

    no message

--- ambient.js
+++ ambient.js
@@ -1,31 +1,41 @@
 //
 $(document).ready(function(){
+
+	var urlWsnNews = "https://blahdns-proxy-eusudefuvv.now.sh/http://140.124.184.204:8080/Cloud/WSN/NewestData";
+
+	var fetchWsnNews = url => {
+		return fetch(url).then(resp => resp.json()).then(function(wsn_news) {
+			console.log(wsn_news);
+			appendWsnNews(wsn_news);
+		});
+	};
+	function appendWsnNews(wsn_news) {
+		var div = document.querySelector(".bubbel");
+		wsn_news.forEach(e => {
+		var temperature = String(e.temperature);
+		var mote = String(e.mote);
+		var id = String(e.id);
+		console.log(id);
+		div.innerHTML += `
+			id:${id}<br>
+			現在溫度 ${temperature} &#8451;<br>
+			`
+		});
+		
+	}
+
+
+
+	fetchWsnNews(urlWsnNews);
+
 	$('.sensor').click(function(){
 		var href = $(this).attr('href');
+		var dataId = $(href).attr('data-id');
+		console.log(dataId);
 		$(href).toggle();
 	});
+
 });
 
-var urlWsnNews = "https://blahdns-proxy-eusudefuvv.now.sh/http://140.124.184.204:8080/Cloud/WSN/NewestData";
 
-var fetchWsnNews = url => {
-	return fetch(url).then(resp => resp.json()).then(function(wsn_news) {
-		console.log(wsn_news);
-		appendWsnNews(wsn_news);
-	});
-};
 
-fetchWsnNews(urlWsnNews);
-
-function appendWsnNews(wsn_news) {
-	var div = document.querySelector(".bubbel");
-	// console.log(wsn_page.length);
-	wsn_news.forEach(e => {
-	var temperature = String(e.temperature);
-	var mote = String(e.mote);
-	div.innerHTML += `
-	溫度感測器 ${mote} <br>
-	溫度 ${temperature} &#8451;<br>
-	`
-	});
-}
