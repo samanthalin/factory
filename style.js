b6cee289b5aafc9d1bef5cdfae43b59b53746909
samantha lin <samanthalin1992@gmail.com>
2017-08-29 06:00:58 +0800

    warning done

--- style.js
+++ style.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#page_buttons span:first-child').addClass('active');
 
 
-    const button = document.getElementById('get_button')
+    const button = document.getElementById('get_button');
 
     button.addEventListener('click', function() {
         setTimeout(function() {
@@ -24,14 +24,22 @@ $(document).ready(function() {
                         item.classList.remove('active')
                     })
                     this.classList.toggle("active")
-
-
                 }
             }
-
         }
+    });
 
+    //scroll to top
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+          $('.scroll-to-top').fadeOut();
+        }
+    });
 
-
-    })
+    $('.scroll-to-top').on('click', function(e) {
+      e.preventDefault();
+        $('html, body').animate({scrollTop : 0}, 800);
+    });
 })
