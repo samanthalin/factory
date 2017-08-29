7a27652de9b9e252b1b9518d6996db825a2e6198
samantha lin <samanthalin1992@gmail.com>
2017-08-30 01:38:32 +0800

    ios done

--- motor.js
+++ motor.js
@@ -1,83 +1,82 @@
 $(document).ready(function() {
 
     var url = "https://blahdns.herokuapp.com/http://140.124.184.204:8080/Cloud/Iotivity/QueryAll",
-        container = $("#container"),
-        voltage = [];
-    $.ajax({
-        "url": url,
-        "type": "get",
-        "dataType": "json"
-    }).done(function(data) {
+          container = $("#container"),
+          voltage = [];
+      $.ajax({
+        "url" : url,
+        "type" : "get",
+        "dataType" : "json"
+      }).done(function(data){
         container.text("Parsing Data...");
         voltage = parseData(data);
-        createChart();
-    })
+        createChart();  
+      })
 
 
-    var parseData = function(data, series) {
+      var parseData = function(data, series){
         var tempdata = [];
-        for (i = data.length - 1; i >= 0; i--) {
-            var freq = data[i],
-                timestamp = freq.timeStamp.replace("-", ""),
-                temp = "",
-                dFormat = [4, 2, 2, 2, 2, 6],
-                spacer = ["-", "-", " ", ":", ":", ""],
-                idx = 0;
-            dFormat.map(function(x, y) {
-                temp = temp + timestamp.substr(idx, x) + spacer[y];
-                //console.log(temp)
-                idx = idx + x;
-                //console.log(idx)
-            })
-            var ts = new Date(temp).getTime(),
-                val = Math.abs(parseFloat(freq.voltage[0].sensorValue.toFixed(3)));
-            tempdata.push([ts, val]);
+        for(i = data.length - 1;i >= 0 ; i--){
+          var freq = data[i],
+              timestamp = freq.timeStamp.replace("-","").replace(".",""),
+              temp = [],
+              dFormat = [4,2,2,2,2,2,3],
+              idx = 0;
+          dFormat.map(function(x,y){
+            var val = parseInt(timestamp.substr(idx,x));
+            temp.push(val);
+            idx = idx + x;
+          })
+          var ts = Date.UTC(temp[0],temp[1]-1,temp[2],temp[3]-8,temp[4],temp[5],temp[6]),
+              val = Math.abs(parseFloat(freq.voltage[0].sensorValue.toFixed(3)));
+          tempdata.push([ts, val]);
         }
         return tempdata;
-    }
+      }
 
-    Highcharts.setOptions({
-        global: {
-            useUTC: false
-        }
-    });
+      Highcharts.setOptions({
+          global: {
+              useUTC: false
+          }
+      });
 
-    var createChart = function() {
+      var createChart = function(){
+        container.text("Loading Chart...");
 
-        // Create the chart
+      // Create the chart
         Highcharts.stockChart('container', {
             chart: {
                 events: {
-                    load: function() {
+                    load: function () {
 
                         // set up the updating of the chart each second
                         var series = this.series[0];
-                        setInterval(function() {
-                            $.ajax({
-                                "url": url,
-                                "type": "get",
-                                "dataType": "json"
-                            }).done(function(data) {
-                                dt = parseData(data);
-                                series.setData(dt, true, "mixed", true);
-                            })
+                        setInterval(function () {
+                          $.ajax({
+                            "url" : url,
+                            "type" : "get",
+                            "dataType" : "json"
+                          }).done(function(data){
+                            dt = parseData(data);
+                            series.setData(dt,true,"mixed",true);
+                          }) 
                         }, 100);
                     }
                 }
             },
 
-            // title: {
-            //     text: '電壓-微秒'
-            // },
+          // title: {
+          //     text: 'Machine Voltage'
+          // },
 
-            exporting: {
-                enabled: true,
-            },
+          exporting: {
+              enabled: false
+          },
 
-            series: [{
-                name: 'Machine Voltage',
-                data: voltage
-            }]
-        });
+          series: [{
+              name: 'Machine Voltage',
+              data: voltage
+          }]
+      });
     }
 });
